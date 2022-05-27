ef40ae76602c91ee5047d1db511d47ce984703b4
doJester13 <minarini.work@gmail.com>
2022-05-27 11:06:59 +0200

    positioning logic

---
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour {
+    public GameObject objectToSpawn;
+
+    ARRaycastManager raycastManager;
+
+
+
+    void Awake() {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                var touchPostion = touch.position;
+
+                List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+                if (raycastManager.Raycast(touchPostion, hitResults, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon)) {
+                    var hitPose = hitResults[0].pose;
+                    Instantiate(objectToSpawn, hitPose.position, hitPose.rotation);
+                }
+            }
+
+        }
+    }
+}
