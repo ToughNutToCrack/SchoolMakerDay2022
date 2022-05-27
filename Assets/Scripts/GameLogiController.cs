6ce361fe2759e7eed00ec07f43fa2eec85c4dc90
doJester13 <minarini.work@gmail.com>
2022-05-27 12:32:35 +0200

    throw of the sphere

--- Assets/Scripts/GameLogiController.cs
+++ Assets/Scripts/GameLogiController.cs
@@ -6,31 +6,58 @@ using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class GameLogiController : MonoBehaviour {
-    public GameObject objectToSpawn;
+
+    public Transform cameraTransform;
+    [Space]
+    public GameObject pokemon;
+    public GameObject pokeball;
+    [Space]
+    public float strength = 1;
+
 
     ARRaycastManager raycastManager;
-    GameObject spawnedObject;
+    ARPlaneManager planeManager;
+    GameObject spawnedPokemon;
 
 
     void Awake() {
         raycastManager = GetComponent<ARRaycastManager>();
+        planeManager = GetComponent<ARPlaneManager>();
     }
 
     void Update() {
-        if (spawnedObject == null) {
-            if (Input.touchCount > 0) {
-                Touch touch = Input.GetTouch(0);
-                if (touch.phase == TouchPhase.Began) {
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+
+                if (spawnedPokemon == null) {
                     var touchPos = touch.position;
 
                     List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
 
                     if (raycastManager.Raycast(touchPos, hitResults, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon)) {
                         var hitPose = hitResults[0].pose;
-                        spawnedObject = Instantiate(objectToSpawn, hitPose.position, hitPose.rotation);
+                        spawnedPokemon = Instantiate(pokemon, hitPose.position, hitPose.rotation);
+                        disablePlaneDetection();
                     }
+                } else {
+                    var spawnPos = cameraTransform.position + cameraTransform.forward * 0.1f;
+                    var pokeballObject = Instantiate(pokeball, spawnPos, cameraTransform.rotation);
+                    var rb = pokeballObject.GetComponent<Rigidbody>();
+                    rb.AddForce((cameraTransform.forward + Vector3.up) * strength, ForceMode.Impulse);
                 }
             }
         }
+    }
+
+    void disablePlaneDetection() {
+        planeManager.enabled = false;
+        setAllPlanesActive(false);
+    }
+
+    void setAllPlanesActive(bool value) {
+        foreach (var plane in planeManager.trackables)
+            plane.gameObject.SetActive(value);
     }
 }
